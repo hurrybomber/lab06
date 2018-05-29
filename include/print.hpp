9aa46f3f60d419802d038e12c1837d2d3a3806f1
hurrybomber <37066949+hurrybomber@users.noreply.github.com>
2018-05-29 19:15:18 +0300

    added sources

--- include/print.hpp
+++ include/print.hpp
@@ -3,4 +3,4 @@
 #include <iostream>
 
 void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ofstream& out); 
